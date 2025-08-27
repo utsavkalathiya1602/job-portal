f2e06a0cbd53640099d9a638729e59c42c40ceb2
Utsav Kalathiya <utsavkalathiya1602@gmail.com>
2025-08-27 16:59:59 +0530

    changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import connectDB from "./src/db/connectDB.js";
 import userRoutes from "./src/routes/userRoutes.js";
 import companyRoutes from "./src/routes/companyRoutes.js";
 import jobRoutes from "./src/routes/jobRoutes.js";
-import Cloudinary from "./src/utils/cloudinary.js";
+import Cloudinary from "./utils/cloudinary.js";
 
 const app = express();
 
--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 
-const Cloudinary = () => {
+const Cloudinary = () => {  
   cloudinary.config({
     cloud_name:"dtennrzvm",
     api_key: 844181244136361,
